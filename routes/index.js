4d89c6f0af197c9b0e38cc301315b5c2182b742c
Allen Day <allenday0114@gmail.com>
2021-09-28 14:07:30 -0400

    nav/routes update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+router.get('/service', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
